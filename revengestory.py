0b5b27dbd1d96843a22487bcc14e82ceedb21e15
osmogfx <137156961+osmogfx@users.noreply.github.com>
2025-11-10 00:24:38 +0500

    Update keywords list in revengestory.py

--- revengestory.py
+++ revengestory.py
@@ -16,13 +16,8 @@ days = st.number_input("Enter Days to Search (1-30):", min_value=1, max_value=30
 
 # List of broader keywords
 keywords = [
- "Affair Relationship Stories", "Reddit Update", "Reddit Relationship Advice", "Reddit Relationship", 
-"Reddit Cheating", "AITA Update", "Open Marriage", "Open Relationship", "X BF Caught", 
-"Stories Cheat", "X GF Reddit", "AskReddit Surviving Infidelity", "GurlCan Reddit", 
-"Cheating Story Actually Happened", "Cheating Story Real", "True Cheating Story", 
-"Reddit Cheating Story", "R/Surviving Infidelity", "Surviving Infidelity", 
-"Reddit Marriage", "Wife Cheated I Can't Forgive", "Reddit AP", "Exposed Wife", 
-"Cheat Exposed"
+"revenge stories", "tales of revenge", "reddit revenge stories", "family drama stories", 
+ "drama stories", "family drama", "family revenge", "stories", "story", "revenge story", "reddit"
 ]
 
 # Fetch Data Button
